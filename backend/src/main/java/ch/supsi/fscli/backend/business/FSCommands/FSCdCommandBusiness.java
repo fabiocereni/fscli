b63e4607fb11fd09fe97da4a5350c79818e574fd
Mattia <mattia.ferraris@student.supsi.ch>
2025-11-18 14:58:03 +0100

    removed comments

--- backend/src/main/java/ch/supsi/fscli/backend/business/FSCommands/FSCdCommandBusiness.java
+++ backend/src/main/java/ch/supsi/fscli/backend/business/FSCommands/FSCdCommandBusiness.java
@@ -34,46 +34,5 @@ public class FSCdCommandBusiness implements IFSCdCommandBusiness {
         }
         stateBusiness.setCurrentWorkingDirectory((DirectoryBusiness) targetNode);
         return true;
-
-        /*
-        if (name == null || name.isBlank())
-            return false;
-
-        DirectoryBusiness currentDir = stateBusiness.getCurrentWorkingDirectory();
-        DirectoryBusiness rootDir = stateBusiness.getRoot();
-
-        if (currentDir == null || rootDir == null)
-            return false;
-
-        if (name.equals("/")) {
-            stateBusiness.setCurrentWorkingDirectory(rootDir);
-            return true;
-        }
-
-        if (name.equals("..")) {
-            IDirectoryBusiness parent = currentDir.getParent();
-            if (parent != null)
-                stateBusiness.setCurrentWorkingDirectory((DirectoryBusiness) parent);
-            return true;
-        }
-
-        Optional<INode> targetNode = currentDir.getContent().stream()
-                .filter(node -> node.getName().equals(name))
-                .findFirst();
-
-        if (targetNode.isEmpty()) {
-            System.out.println("cd: directory not found '" + name + "'");
-            return false;
-        }
-
-        INode target = targetNode.get();
-        if (target.getType() != NodeType.DIRECTORY) {
-            System.out.println("cd: not a directory: '" + name + "'");
-            return false;
-        }
-
-        stateBusiness.setCurrentWorkingDirectory((DirectoryBusiness) target);
-        return true;
-         */
     }
 }
