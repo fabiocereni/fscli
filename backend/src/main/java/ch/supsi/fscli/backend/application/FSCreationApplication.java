0cd5d75ed8adc751e842dd0bb97bccfadd8007d4
simone.torri <simone.torri@supsi.ch>
2025-11-25 18:16:03 +0100

    fix

--- backend/src/main/java/ch/supsi/fscli/backend/application/FSCreationApplication.java
+++ backend/src/main/java/ch/supsi/fscli/backend/application/FSCreationApplication.java
@@ -2,9 +2,7 @@ package ch.supsi.fscli.backend.application;
 
 import ch.supsi.fscli.backend.business.FSCreationBusiness;
 import ch.supsi.fscli.backend.business.IFSCreationBusiness;
-import com.google.inject.Singleton;
 
-@Singleton
 public class FSCreationApplication implements IFSCreationApplication {
 
     private final IFSCreationBusiness fsCreationBusiness = FSCreationBusiness.getInstance();
