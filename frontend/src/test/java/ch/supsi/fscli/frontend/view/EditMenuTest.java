200116408f3f27a372c108f31fe2dfbd649eb866
simone.torri <simone.torri@student.supsi.ch>
2025-12-15 11:13:52 +0100

    test end-to-end bug fix

--- frontend/src/test/java/ch/supsi/fscli/frontend/view/EditMenuTest.java
+++ frontend/src/test/java/ch/supsi/fscli/frontend/view/EditMenuTest.java
@@ -1,62 +1,71 @@
-//package ch.supsi.fscli.frontend.view;
-//
-//import javafx.scene.control.TextField;
-//import javafx.scene.control.TextInputControl;
-//import javafx.scene.input.KeyCode;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.testfx.api.FxAssert.verifyThat;
-//import static org.testfx.matcher.base.NodeMatchers.isDisabled;
-//import static org.testfx.matcher.base.NodeMatchers.isEnabled;
-//import static org.testfx.util.NodeQueryUtils.isVisible;
-//
-//public class EditMenuTest extends AbstractMainGUITest {
-//
-//
-//    @Test
-//    public void testPreferences() {
-//        step("Apertura finestra preferenze...", () -> {
-//            clickOn("#editMenu");
-//
-//
-//            clickOn("#preferencesMenuItem");
-//
-//            verifyThat("#languageComboBox", isVisible());
-//            verifyThat("#linesField", isVisible());
-//
-//
-//            verifyThat("#saveButton", isDisabled());
-//        });
-//
-//        step("Modifica valori e abilitazione tasto Salva...", () -> {
-//            doubleClickOn("#linesField");
-//            TextField numberOfLines = lookup("#linesField").queryAs(TextField.class);
-//
-//            String tmp = numberOfLines.getText();
-//
-//            int num = Integer.parseInt(tmp);
-//
-//            int numToWrite = num + 5;
-//
-//            write(String.valueOf(numToWrite));
-//
-//            press(KeyCode.TAB).release(KeyCode.TAB);
-//
-//            verifyThat("#saveButton", isEnabled());
-//        });
-//
-//        step("Salvataggio e chiusura...", () -> {
-//            clickOn("#saveButton");
-//
-//
-//            verifyThat("#logView", (TextInputControl t) ->
-//                    t.getText().contains("Preferenze salvate correttamente.") ||
-//                            t.getText().contains("Preferences saved successfully.") ||
-//                            t.getText().contains("Einstellungen erfolgreich gespeichert.")
-//            );
-//        });
-//    }
-//
-//
-//
-//}
+package ch.supsi.fscli.frontend.view;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.input.KeyCode;
+import org.junit.jupiter.api.Test;
+
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.util.NodeQueryUtils.isVisible;
+
+public class EditMenuTest extends AbstractMainGUITest {
+
+
+    @Test
+    public void testPreferences() {
+        step("Apertura finestra preferenze...", () -> {
+            clickOn("#editMenu");
+
+            sleep(SLEEP_INTERVAL);
+
+            clickOn("#preferencesMenuItem");
+            sleep(SLEEP_INTERVAL);
+
+            verifyThat("#linesField", isVisible());
+
+
+            verifyThat("#saveButton", isDisabled());
+        });
+
+        step("Modifica valori e abilitazione tasto Salva...", () -> {
+            doubleClickOn("#linesField");
+            sleep(SLEEP_INTERVAL);
+            TextField numberOfLines = lookup("#linesField").queryAs(TextField.class);
+
+            String tmp = numberOfLines.getText();
+
+            int num = Integer.parseInt(tmp);
+
+            int numToWrite;
+
+            if(num > 10)
+                numToWrite = num+10;
+            else
+                numToWrite = num-10;
+
+            write(String.valueOf(numToWrite));
+
+            press(KeyCode.TAB).release(KeyCode.TAB);
+            sleep(SLEEP_INTERVAL);
+
+            verifyThat("#saveButton", isEnabled());
+        });
+
+        step("Salvataggio e chiusura...", () -> {
+            clickOn("#saveButton");
+
+            sleep(SLEEP_INTERVAL);
+
+            verifyThat("#logView", (TextInputControl t) ->
+                    t.getText().contains("Preferenze salvate correttamente.") ||
+                            t.getText().contains("Preferences saved successfully.") ||
+                            t.getText().contains("Einstellungen erfolgreich gespeichert.")
+            );
+        });
+    }
+
+
+
+}
--- frontend/src/test/java/ch/supsi/fscli/frontend/view/FileMenuTest.java
+++ frontend/src/test/java/ch/supsi/fscli/frontend/view/FileMenuTest.java
@@ -1,109 +1,121 @@
-//package ch.supsi.fscli.frontend.view;
-//
-//import com.sun.javafx.scene.control.ContextMenuContent;
-//
-//import javafx.scene.control.MenuItem;
-//import javafx.scene.control.TextInputControl;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.testfx.api.FxAssert.verifyThat;
-//import static org.testfx.matcher.base.NodeMatchers.*;
-//
-//public class FileMenuTest extends AbstractMainGUITest {
-//
-//    public void createNew() {
-//        clickOn("#fileMenu");
-//        clickOn("#newMenuItem");
-//    }
-//
-//    @Test
-//    public void testNewButtonPressed() {
-//        step("Test creazione nuovo FS...", () -> {
-//            createNew();
-//
-//            clickOn("#fileMenu");
-//
-//            MenuItem exitMenuItem = lookup("#exitMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
-//            assertTrue(exitMenuItem.isVisible());
-//            assertFalse(exitMenuItem.isDisable());
-//
-//
-//            clickOn("#fileMenu");
-//
-//            verifyThat("#enter", isVisible());
-//            verifyThat("#enter", isEnabled());
-//            verifyThat("#outputView", (TextInputControl t) -> t.getText().isEmpty());
-//            verifyThat("#logView", (TextInputControl t) -> t.getText().contains("FS Creato con successo.") ||
-//                    t.getText().contains("FS created successfully.") ||
-//                    t.getText().contains("Dateisystem erfolgreich erstellt."));
-//        });
-//    }
-//    @Test
-//    public void testOpenMenuItem() {
-//        step("Test Open...", () -> {
-//            clickOn("#fileMenu");
-//
-//            MenuItem openMenuItem = lookup("#openMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
-//            assertTrue(openMenuItem.isVisible());
-//            assertFalse(openMenuItem.isDisable());
-//
-//
-//            clickOn("#fileMenu");
-//        });
-//    }
-//    @Test
-//    public void testSaveAsMenuItem() {
-//        step("Test Save As...", () -> {
-//
-//            clickOn("#fileMenu");
-//            MenuItem saveAsItem = lookup("#saveAsMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
-//            assertTrue(saveAsItem.isVisible());
-//            assertTrue(saveAsItem.isDisable());
-//            clickOn("#fileMenu");
-//
-//
-//            createNew();
-//
-//
-//            clickOn("#fileMenu");
-//            MenuItem saveAsItemEnabled = lookup("#saveAsMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
-//            assertFalse(saveAsItemEnabled.isDisable());
-//
-//            clickOn("#fileMenu");
-//        });
-//    }
-//
-//    @Test
-//    public void testSaveMenuItem() {
-//        step("test save menu item...", () -> {
-//             createNew();
-//
-//            clickOn("#fileMenu");
-//            verifyThat("#saveMenuItem", isVisible());
-//
-//            clickOn("#saveMenuItem");
-//
-//            verifyThat("#logView", (TextInputControl t) ->
-//                    t.getText().contains("File system salvato correttamente in") ||
-//                            t.getText().contains("FS saved successfully in") ||
-//                            t.getText().contains("Dateisystem erfolgreich gespeichert in")
-//            );
-//        });
-//    }
-//
-//    @Test
-//    public void testExitMenuItem() {
-//        step("Test voce menu Exit...", () -> {
-//            clickOn("#fileMenu");
-//
-//            MenuItem exitMenuItem = lookup("#exitMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
-//
-//            assertTrue(exitMenuItem.isVisible(), "Il menu Exit dovrebbe essere visibile");
-//            assertFalse(exitMenuItem.isDisable(), "Il menu Exit dovrebbe essere sempre abilitato");
-//
-//            clickOn("#fileMenu");
-//        });
-//    }
-//}
+package ch.supsi.fscli.frontend.view;
+
+import com.sun.javafx.scene.control.ContextMenuContent;
+
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextInputControl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.base.NodeMatchers.*;
+
+public class FileMenuTest extends AbstractMainGUITest {
+
+    public void createNew() {
+        sleep(SLEEP_INTERVAL);
+        clickOn("#fileMenu");
+        sleep(SLEEP_INTERVAL);
+        clickOn("#newMenuItem");
+        sleep(SLEEP_INTERVAL);
+    }
+
+    @Test
+    public void testNewButtonPressed() {
+        step("Test creazione nuovo FS...", () -> {
+            createNew();
+
+            clickOn("#fileMenu");
+
+            MenuItem exitMenuItem = lookup("#exitMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+            assertTrue(exitMenuItem.isVisible());
+            assertFalse(exitMenuItem.isDisable());
+
+            sleep(SLEEP_INTERVAL);
+
+
+            clickOn("#fileMenu");
+
+            verifyThat("#enter", isVisible());
+            verifyThat("#enter", isEnabled());
+            verifyThat("#outputView", (TextInputControl t) -> t.getText().isEmpty());
+            verifyThat("#logView", (TextInputControl t) ->
+                                        t.getText().contains("FS creato con successo.") ||
+                                        t.getText().contains("FS created successfully.") ||
+                                        t.getText().contains("Dateisystem erfolgreich erstellt."));
+        });
+    }
+    @Test
+    public void testOpenMenuItem() {
+        step("Test Open...", () -> {
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+
+            MenuItem openMenuItem = lookup("#openMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+            assertTrue(openMenuItem.isVisible());
+            assertFalse(openMenuItem.isDisable());
+
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+        });
+    }
+    @Test
+    public void testSaveAsMenuItem() {
+        step("Test Save As...", () -> {
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+            MenuItem saveAsItem = lookup("#saveAsMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+            assertTrue(saveAsItem.isVisible());
+            assertTrue(saveAsItem.isDisable());
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+
+
+            createNew();
+            sleep(SLEEP_INTERVAL);
+
+            clickOn("#fileMenu");
+            MenuItem saveAsItemEnabled = lookup("#saveAsMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+            assertFalse(saveAsItemEnabled.isDisable());
+
+            sleep(SLEEP_INTERVAL);
+
+            clickOn("#fileMenu");
+        });
+    }
+
+    @Test
+    public void testSaveMenuItem() {
+        step("test save menu item...", () -> {
+             createNew();
+
+            clickOn("#fileMenu");
+            sleep(SLEEP_INTERVAL);
+            verifyThat("#saveMenuItem", isVisible());
+
+            clickOn("#saveMenuItem");
+
+            sleep(SLEEP_INTERVAL);
+            verifyThat("#logView", (TextInputControl t) ->
+                    t.getText().contains("File system salvato correttamente in") ||
+                            t.getText().contains("FS saved successfully in") ||
+                            t.getText().contains("Dateisystem erfolgreich gespeichert in")
+            );
+        });
+    }
+
+    @Test
+    public void testExitMenuItem() {
+        step("Test voce menu Exit...", () -> {
+            clickOn("#fileMenu");
+            sleep(SLEEP_INTERVAL);
+            MenuItem exitMenuItem = lookup("#exitMenuItem").queryAs(ContextMenuContent.MenuItemContainer.class).getItem();
+
+            assertTrue(exitMenuItem.isVisible(), "Il menu Exit dovrebbe essere visibile");
+            assertFalse(exitMenuItem.isDisable(), "Il menu Exit dovrebbe essere sempre abilitato");
+            sleep(SLEEP_INTERVAL);
+            clickOn("#fileMenu");
+        });
+    }
+}
